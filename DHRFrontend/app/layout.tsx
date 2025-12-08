5d43b334402ebc42d5ae0795c441d047c76d2ae2
PRASAD <prasadpalei360@gmail.com>
2025-12-08 16:46:32 +0530

    final regsition

--- DHRFrontend/app/layout.tsx
+++ DHRFrontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 import { Suspense } from "react"
 import { ApiProvider } from "../context/ApiContext"
@@ -34,6 +35,7 @@ export default function RootLayout({
           }>
             {children}
           </Suspense>
+          <Analytics />
         </ApiProvider>
       </body>
     </html>
